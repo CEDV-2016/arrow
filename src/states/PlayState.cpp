2fcc340cacb68fd98a4e62bb70a8254ed03169e8
Jota <josejuanr27@hotmail.com>
2017-05-18 17:35:52 +0200

    [Camera] Add basic movement

--- src/states/PlayState.cpp
+++ src/states/PlayState.cpp
@@ -22,13 +22,31 @@ PlayState::enter ()
   _mapManager = MapManager::getSingletonPtr();
 
   _sceneMgr->clearScene(); //deleting background image
-  _sceneMgr->getCamera("MainCamera")->setPosition( Ogre::Vector3(-12, 10, 12) );
-  _sceneMgr->getCamera("MainCamera")->lookAt( Ogre::Vector3(-5, 5, -9) );
+  Ogre::Camera* cam = _sceneMgr->getCamera("MainCamera");
+  cam->setPosition( Ogre::Vector3(0, 0, 0) );
+  //_sceneMgr->getCamera("MainCamera")->lookAt( Ogre::Vector3(-5, 5, -9) );
+  cam->setNearClipDistance(5);
+  cam->setFarClipDistance(10000);
 
   createScene();
   createHUD();
 
+  Ogre::SceneNode *_node = _sceneMgr->getSceneNode("Player");
+  this->cameraNode = _node->createChildSceneNode();
+  this->cameraNode->setPosition(0, 0, 0);
+  // Create the camera's yaw node as a child of camera's top node.
+  this->cameraYawNode = this->cameraNode->createChildSceneNode();
+  //Ogre::Entity* playerEntity = _sceneMgr->createEntity("Player", "Player.mesh");
+  //this->cameraYawNode->attachObject(playerEntity);
+  // Create the camera's pitch node as a child of camera's yaw node.
+  this->cameraPitchNode = this->cameraYawNode->createChildSceneNode();
+  this->cameraPitchNode->attachObject(this->_camera);
 
+  moveScale = 0.05f;
+  rotateScale = 0.013;
+  rotX = 0;
+  rotY = 0;
+  translateVector = Ogre::Vector3::ZERO;
 
   _exitGame = false;
 }
@@ -81,6 +99,32 @@ void
 PlayState::keyPressed
 (const OIS::KeyEvent &e)
 {
+  if (e.key == OIS::KC_UP || e.key == OIS::KC_DOWN || e.key ==OIS::KC_LEFT ||
+    e.key ==OIS::KC_RIGHT) {
+    if(e.key == OIS::KC_UP){
+      this->translateVector.z = -(this->moveScale);
+    }
+
+    // Move camera backward.
+    if(e.key == OIS::KC_DOWN){
+      this->translateVector.z = this->moveScale;
+    }
+
+    // Move camera left.
+    if(e.key == OIS::KC_LEFT){
+      this->translateVector.x = -(this->moveScale);
+    }
+
+    // Move camera right.
+    if(e.key == OIS::KC_RIGHT){
+      this->translateVector.x = this->moveScale;
+    }
+    this->cameraNode->translate(this->cameraYawNode->getOrientation() *
+	 														 translateVector,
+	                             Ogre::SceneNode::TS_LOCAL);
+    translateVector = Ogre::Vector3::ZERO;
+  }
+  //moveCamera();
 }
 
 void
@@ -94,6 +138,22 @@ void
 PlayState::mouseMoved
 (const OIS::MouseEvent &e)
 {
+  rotX = Ogre::Degree(-e.state.X.rel * 0.13);
+  rotY = Ogre::Degree(-e.state.Y.rel * 0.13);
+
+  // Yaws the camera according to the mouse relative movement.
+	this->cameraYawNode->yaw(this->rotX);
+
+  // Pitches the camera according to the mouse relative movement.
+	Ogre::Real pitchAng = (2 * Ogre::Degree(Ogre::Math::ACos(this->cameraPitchNode->getOrientation().w)).valueDegrees());
+  Ogre::Real pitchAngSig = this->cameraPitchNode->getOrientation().x;
+  Ogre::Real pitchDegree = rotY.valueDegrees();
+  if (((pitchAng < 15.0f && pitchAngSig >= 0) ||
+ 		 (pitchAng > 15.0f && pitchAngSig >= 0 && pitchDegree < 0)) ||
+ 		  ((pitchAng < 10.0f && pitchAngSig < 0) ||
+      (pitchAng > 10.0f && pitchAngSig < 0 && pitchDegree > 0))) {
+	 			this->cameraPitchNode->pitch(this->rotY);
+	 }
 }
 
 void
@@ -124,6 +184,9 @@ PlayState::getSingleton ()
 void PlayState::createScene()
 {
   _mapManager->changeMap( enumerations::Maps::ROOM, false );
+  Ogre::SceneNode* nodePlayer = _sceneMgr->createSceneNode("Player");
+  nodePlayer->setScale(3.5,3.5,3.5);
+  _sceneMgr->getRootSceneNode()->addChild(nodePlayer);
 }
 
 void PlayState::createHUD()
@@ -134,6 +197,30 @@ void PlayState::createHUD()
   MyOverlayManager::getSingletonPtr()->setTime( "1.59" );
   MyOverlayManager::getSingletonPtr()->setDianasLeft( "x6" );
   MyOverlayManager::getSingletonPtr()->setArrowsLeft( "x4" );
+}
+
+void PlayState::moveCamera()
+{
+  //Ogre::Real pitchAngle;
+  //Ogre::Real pitchAngleSign;
+
+  //const OIS::MouseState &ms = _mouse->getMouseState();
+  //rotX = Ogre::Degree(-ms.X.rel * 0.13);
+  //rotY = Ogre::Degree(-ms.Y.rel * 0.13);
+
+  // Yaws the camera according to the mouse relative movement.
+	 //this->cameraYawNode->yaw(this->rotX);
+
+	 // Pitches the camera according to the mouse relative movement.
+	 /*Ogre::Real pitchAng = (2 * Ogre::Degree(Ogre::Math::ACos(this->cameraPitchNode->getOrientation().w)).valueDegrees());
+	 Ogre::Real pitchAngSig = this->cameraPitchNode->getOrientation().x;
+	 Ogre::Real pitchDegree = rotY.valueDegrees();
+	 if (((pitchAng < 15.0f && pitchAngSig >= 0) ||
+	 		(pitchAng > 15.0f && pitchAngSig >= 0 && pitchDegree < 0)) ||
+	 		((pitchAng < 10.0f && pitchAngSig < 0) ||
+			(pitchAng > 10.0f && pitchAngSig < 0 && pitchDegree > 0))) {
+	 			this->cameraPitchNode->pitch(this->rotY);
+	 }*/
 }
 
 void PlayState::setPlayerName(std::string name)
